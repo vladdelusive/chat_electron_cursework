eed5a882bc3ea513758e963420441934939637ab
vladdelusive <mr.tovstochub@mail.ru>
2020-11-15 11:33:42 +0200

    add: notifications redux store

---
+++ src/store/notifications/sagas.js
@@ -0,0 +1,10 @@
+import { takeEvery } from 'redux-saga/effects';
+import * as notification from './types';
+
+function* changeStatusOnlineSaga() {
+
+}
+
+export function* notificationSaga() {
+    yield takeEvery(notification.CHANGE_STATUS, changeStatusOnlineSaga);
+}
