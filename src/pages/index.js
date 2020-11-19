e7892b25cc327d092ffc7ce16c68f3ac32d15ba5
vladdelusive <mr.tovstochub@mail.ru>
2020-11-19 11:52:38 +0200

    refactor: pages exports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 export { PageHome } from './home';
 export { PageProfile } from './profile';
-export { PageContacts } from './contacts';
 export { NotFound } from './not-found';
-export { PageContact } from './contact';
+export { PageChats } from './chats';
+export { PageProfiles } from './profiles';
