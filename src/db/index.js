597aa543575bdaeb35e3ea71a8a61e07606c75ed
vladdelusive <mr.tovstochub@mail.ru>
2020-11-20 11:53:35 +0200

    add: db to project

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const config = {
+    apiKey: "AIzaSyDh8jwIC1dr8cznmteS3jGgrCW-pmuQAKE",
+    authDomain: "electron-chat-test-47c35.firebaseapp.com",
+    databaseURL: "https://electron-chat-test-47c35.firebaseio.com",
+    projectId: "electron-chat-test-47c35",
+    storageBucket: "electron-chat-test-47c35.appspot.com",
+    messagingSenderId: "1006786790325",
+    appId: "1:1006786790325:web:dc7a12822cda97686a8ffc",
+    measurementId: "G-YJ0E1D34G2"
+};
