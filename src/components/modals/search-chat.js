c11496c9750faec0c05f77417e0964626586f19d
vladdelusive <mr.tovstochub@mail.ru>
2020-11-25 12:06:56 +0200

    feat: search modal chat - filter search + list of users

--- src/components/modals/search-chat.js
+++ src/components/modals/search-chat.js
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux';
 import { compose } from 'redux';
-import { Modal, Row, Col, Input } from 'antd';
+import { Modal, List, Skeleton, Avatar, Row, Col, Input, Button, Tag } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { getIsLoadingUsersChats, getUsersChats } from 'store/chats/selectors';
 import { createNewChat, fetchUsersForChat } from 'store/chats/actions';
+import { Link } from 'react-router-dom';
+import { routes } from 'routes';
 
 function SearchChatModal(props) {
-    const { setIsShowNewChatModal, fetchUsersForChat } = props;
+    const { setIsShowNewChatModal, chatList, fetchUsersForChat, loading, createNewChat } = props;
 
     const [isShow, setIsShow] = useState(true)
     const [searchValue, setSearchValue] = useState("")
 
+    const filteredChatsList = (searchValue?.toString().trim().length && chatList.filter(({ name, email }) => {
+        return name?.toString().toLowerCase().trim().includes(searchValue.toString().toLowerCase().trim()) || email?.toString().toLowerCase().trim().includes(searchValue.toString().toLowerCase().trim())
+    })) || chatList;
 
     useEffect(() => {
         fetchUsersForChat()
@@ -36,7 +43,35 @@ function SearchChatModal(props) {
                     />
                 </Col>
             </Row>
-           
+            <List
+                className="chat-list-modal"
+                loading={loading}
+                itemLayout="horizontal"
+                loadMore={false}
+                dataSource={filteredChatsList}
+                renderItem={item => (
+                    <List.Item
+                        actions={[
+                            <Button
+                                size="small"
+                                onClick={() => createNewChat(item.uid)}
+                            >Start chat</Button>
+                        ]}
+                    >
+                        <Skeleton avatar title={false} loading={item.loading} active>
+                            <List.Item.Meta
+                                avatar={
+                                    <Link to={routes["profiles"].link(item.uid)}>
+                                        <Avatar src={item.photo} icon={<UserOutlined />} />
+                                    </Link>
+                                }
+                                title={<Link to={routes["profiles"].link(item.uid)}>{item.name}</Link>}
+                                description={<Tag>{item.email}</Tag>}
+                            />
+                        </Skeleton>
+                    </List.Item>
+                )}
+            />
         </Modal>
     )
 }
@@ -44,6 +79,8 @@ function SearchChatModal(props) {
 
 const mapStateToProps = (state) => {
     return {
+        chatList: getUsersChats(state),
+        loading: getIsLoadingUsersChats(state),
     }
 };
 
