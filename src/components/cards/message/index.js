a1173f7cd5f880144288bf736c691cf8409ba7ce
vladdelusive <mr.tovstochub@mail.ru>
2020-11-27 12:13:09 +0200

    add: message card component

---
+++ src/components/cards/message/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export function MessageCard(props) {
+    const { item } = props;
+    const { 
+        message, 
+        me, 
+        // time, 
+        // id,
+    } = item;
+    return (
+        <div className={`message ${me ? "from-me--message" : "to-me--message"}`}>
+            <div className="message__content">
+                {message}
+            </div>
+        </div>
+    )
+}
