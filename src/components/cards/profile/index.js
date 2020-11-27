3d4650fa24e81f33abf32736125f3f27ff92f9c5
vladdelusive <mr.tovstochub@mail.ru>
2020-11-27 12:12:37 +0200

    add: to profile component additional data

--- src/components/cards/profile/index.js
+++ src/components/cards/profile/index.js
@@ -23,6 +23,34 @@ export const ProfileCard = React.memo((props) => {
                     <Title>{title}</Title>
                 </Col>
             </Row>
+            <Row gutter={16} type={"flex"} justify="center" >
+                <Spin isDataExist={isDataExist} style={{ margin: "60px auto 100px" }} >
+                    <Col className="avatar" style={{ marginBottom: 20 }}>
+                        <img alt="avatar" src={img} />
+                    </Col>
+                    <Col>
+                        <Title level={3}>{nameTitle}&nbsp;
+                            {/* <Text type="secondary" style={{ fontSize: 16 }}>{`(${age} years)`}</Text> */}
+                        </Title>
+                        <Divider dashed />
+                        <Row typeof="flex" gutter={4}>
+                            <Col><Paragraph copyable={{ text: email }} /></Col>
+                            <Col><a href={`mailto:${email}`}>{email}</a></Col>
+                        </Row>
+                        <Divider dashed />
+                    </Col>
+                </Spin>
+                {/* <Row typeof="flex" gutter={4}>
+                        <Col><Paragraph copyable={{ text: number }} /></Col>
+                        <Col><a href={`tel:${number}`}>{number}</a></Col>
+                    </Row>
+                    <Row typeof="flex" gutter={4}>
+                        <Col><Paragraph copyable={{ text: `${country} ${street}` }} /></Col>
+                        <Col><Text strong>/{country}/</Text><br /><span style={{ marginLeft: 5 }}>{street}</span></Col>
+                    </Row>
+                    <Divider dashed />
+                    <Tag color={nation.color}>{nation.name}</Tag> */}
+            </Row>
             {returnBtn ? <Row type={"flex"} justify="center">
                 <Col>
                     <Link to="/chats" >
