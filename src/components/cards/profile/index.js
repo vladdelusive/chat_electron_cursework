0fcc847c0cd568c2f5ff6c2980d8b6e9d53c9d9a
vladdelusive <mr.tovstochub@mail.ru>
2020-11-27 12:12:04 +0200

    feat: profile component

--- src/components/cards/profile/index.js
+++ src/components/cards/profile/index.js
@@ -1,28 +1,21 @@
 import { DoubleLeftOutlined } from '@ant-design/icons';
-import { Button, Col, Divider, Row, Tag, Typography } from 'antd'
+import { Button, Col, Divider, Row, Typography } from 'antd'
 import { Spin } from 'components/common';
-import { NATIONALITIES } from 'constants/nationalities';
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-const { Title, Text, Paragraph } = Typography;
+const { Title, Paragraph } = Typography;
 
 export const ProfileCard = React.memo((props) => {
     const {
         img,
         nameTitle,
         email,
-        age,
-        country,
-        street,
-        nat,
-        number,
         title,
-        isDataExist,
         returnBtn,
+        isDataExist,
     } = props
 
-    const nation = NATIONALITIES[nat] || NATIONALITIES.DEFAULT
     return (
         <div className="page--profile">
             <Row type={"flex"} justify="center">
@@ -30,32 +23,13 @@ export const ProfileCard = React.memo((props) => {
                     <Title>{title}</Title>
                 </Col>
             </Row>
-            <Spin isDataExist={isDataExist}>
-                <Row gutter={16} type={"flex"} justify="center" >
-                    <Col className="avatar" style={{ marginBottom: 20 }}>
-                        <img alt="avatar" src={img} />
-                    </Col>
-                    <Col>
-                        <Title level={3}>{nameTitle}&nbsp;
-                            <Text type="secondary" style={{ fontSize: 16 }}>{`(${age} years)`}</Text>
-                        </Title>
-                        <Divider dashed />
-                        <Paragraph copyable>{email}</Paragraph>
-                        <Paragraph copyable>{number}</Paragraph>
-                        <Paragraph copyable>/{country}/</Paragraph>
-                        <Paragraph>{street}</Paragraph>
-                        <Divider dashed />
-                        <Tag color={nation.color}>{nation.name}</Tag>
-                    </Col>
-                </Row>
-                {returnBtn ? <Row type={"flex"} justify="center">
-                    <Col>
-                        <Link to="/contacts" >
-                            <Button icon={<DoubleLeftOutlined />} type="primary">Back</Button>
-                        </Link>
-                    </Col>
-                </Row> : null}
-            </Spin>
+            {returnBtn ? <Row type={"flex"} justify="center">
+                <Col>
+                    <Link to="/chats" >
+                        <Button icon={<DoubleLeftOutlined />} type="primary">Back</Button>
+                    </Link>
+                </Col>
+            </Row> : null}
         </div>
     )
 })
