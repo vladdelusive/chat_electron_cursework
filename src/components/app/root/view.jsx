f4a692381a1fffd39aa6b5489be3dc2f08706c82
vladdelusive <mr.tovstochub@mail.ru>
2020-11-27 12:14:11 +0200

    feat: root component

--- src/components/app/root/view.jsx
+++ src/components/app/root/view.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { RoutesSwitch } from 'routes';
 import 'antd/dist/antd.css';
 import 'assets/styles/index.scss';
+import { OfflineModal } from 'components/modals';
+import { useSelector } from 'react-redux';
+import { getNotificationOnlineStatus } from 'store/notifications/selectors';
 
 const View = React.memo(() => {
+	const statusOnline = useSelector(state => getNotificationOnlineStatus(state))
+
 	return (
-		<RoutesSwitch />
+		<>
+			{!statusOnline && <OfflineModal />}
+			<RoutesSwitch />
+		</>
 	);
 });
 
