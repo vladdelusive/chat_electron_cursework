905c680512da0117592d4fcad48aa2df4141795d
vladdelusive <mr.tovstochub@mail.ru>
2020-12-04 21:51:42 +0200

    feat: update chat messages

--- src/store/chats/sagas.js
+++ src/store/chats/sagas.js
@@ -1,7 +1,7 @@
 import { call, put, select, takeEvery } from 'redux-saga/effects';
 import * as chats from './types';
 import { api } from 'services';
-import { saveUsersForChat } from './actions';
+import { saveChats, saveUsersForChat } from './actions';
 import { getAuthProfileUid } from 'store/auth/selectors';
 import { getChatsList } from './selectors';
 
@@ -28,9 +28,8 @@ function* fetchUsersForChatSaga() {
         const response = yield call(api.chats.fetchProfiles);
         if (!response) return;
         const userUid = yield select(getAuthProfileUid)
-        const userChats = yield select(getChatsList)
-        const profiles = response.filter(profile => profile.id !== userUid && !userChats.find(chat => chat.userInfo.uid === profile.id))
-        // debugger
+        const chats = yield select(getChatsList)
+        const profiles = response.filter(profile => profile.id !== userUid && !chats.find(chat => chat.userInfo.uid === profile.id))
         yield put(saveUsersForChat(profiles))
     } catch (error) {
         console.warn(error);
@@ -41,8 +40,13 @@ function* setUpdateChatMessageSaga(action) {
     try {
         const { payload } = action;
         const { chatUid, data } = payload;
-        debugger
-        yield console.log("")
+
+        const chats = yield select(getChatsList)
+        const updatedChats = [...chats].map(chat => {
+            if (chat.id === chatUid) return { ...chat, messages: data.messages }
+            return chat
+        })
+        yield put(saveChats(updatedChats))
     } catch (error) {
         console.log(error);
     }
