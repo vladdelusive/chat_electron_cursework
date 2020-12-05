81e5634e94d2236fe4afe9ceef57fd6ae742e2fa
vladdelusive <mr.tovstochub@mail.ru>
2020-12-05 02:19:33 +0200

    fix: errors & style message

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,3 @@
-export {
-	provide,
-	provideWithRandomError,
-} from './provide';
-export {
-	randomIntegerInRange,
-} from './random';
 export { redirect } from './redirect';
 
 export { noty } from './noty'
-
-export { sortObjectToEntriesArray } from './sort'
